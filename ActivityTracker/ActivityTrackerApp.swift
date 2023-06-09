961eba12eb8232abf0721f82c7a98a20b4bbd8f8
Khawlah Khalid <khawlah1920@hotmail.com>
2023-06-09 10:54:39 +0300

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Khawlah Khalid on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
